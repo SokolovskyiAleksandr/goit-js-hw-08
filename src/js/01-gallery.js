8945c9cb8f60f0c1b96e045ca6bf9fa001685d39
Sokolovskyi Aleksandr <a.sokol19892104@gmail.com>
2022-06-29 14:11:15 +0300

    1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,32 @@
+// Описан в документации
+import SimpleLightbox from "simplelightbox";
+// Дополнительный импорт стилей
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+const imageList = document.querySelector(".gallery");
+
+const imageListItem = galleryItems.map(({ preview, original, description }) =>
+
+    `<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+      
+    />
+  </a>
+</div>`
+).join('');
+
+
+imageList.insertAdjacentHTML(`afterbegin`, imageListItem);
+// // console.log(galleryItems);
+
+let gallery = new SimpleLightbox('.gallery a', {captionsData: 'alt',
+captionDelay: 250 });
