271ca30b3efc7804f9f603f9c2fab359fde6951c
Sokolovskyi Aleksandr <a.sokol19892104@gmail.com>
2022-06-30 14:17:37 +0300

    2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,17 @@
+import vimeoPlayer from '@vimeo/player';
+// import throttle from 'lodash.throttle';
+const throttle = require('lodash.throttle');
+const CURRENT_TIME = 'videoplayer-current-time';
+const iframe = document.querySelector('#vimeo-player');
+const player = new vimeoPlayer(iframe);
 
+
+player.on('timeupdate',throttle(timeplaying, 1000));
+
+function timeplaying (event) {
+    const currentTime = event.seconds;
+    localStorage.setItem(CURRENT_TIME, JSON.stringify(currentTime));
+
+    console.log(currentTime);
+}
+player.setCurrentTime(JSON.parse(localStorage.getItem(CURRENT_TIME)));
