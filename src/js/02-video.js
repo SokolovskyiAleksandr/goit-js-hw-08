ac2bc3b962326455b6095747e6de023f823334c8
Sokolovskyi Aleksandr <a.sokol19892104@gmail.com>
2022-07-01 02:01:02 +0300

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -5,13 +5,22 @@ const CURRENT_TIME = 'videoplayer-current-time';
 const iframe = document.querySelector('#vimeo-player');
 const player = new vimeoPlayer(iframe);
 
+player.on('timeupdate', throttle(timeplaying, 1000));
 
-player.on('timeupdate',throttle(timeplaying, 1000));
+function timeplaying(event) {
+  const currentTime = event.seconds;
+  localStorage.setItem(CURRENT_TIME, JSON.stringify(currentTime));
 
-function timeplaying (event) {
-    const currentTime = event.seconds;
-    localStorage.setItem(CURRENT_TIME, JSON.stringify(currentTime));
-
-    console.log(currentTime);
+  console.log(`Время воспроизведения ${currentTime} секунд`);
 }
-player.setCurrentTime(JSON.parse(localStorage.getItem(CURRENT_TIME)));
+// player.setCurrentTime(JSON.parse(localStorage.getItem(CURRENT_TIME)));
+const timeLS = localStorage.getItem(CURRENT_TIME);
+if (timeLS) {
+  const savedTime = JSON.parse(timeLS);
+  if (savedTime) {
+    player.setCurrentTime(savedTime);
+    console.log(`Вы закончили просмотр на ${Math.round(savedTime)} секунде`);
+  }
+} else {
+  console.log(`Вы не просматривали это видео`);
+}
